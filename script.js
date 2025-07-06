585816465d5f3c22bd1e1c09715c59b23967a3d2
NiarDm <158210686+NiarDm@users.noreply.github.com>
2025-07-06 20:45:23 +0300

    Update script.js
    проблема с кодировкой

--- script.js
+++ script.js
@@ -2,46 +2,46 @@ document.addEventListener('DOMContentLoaded', function() {
     const addContactBtn = document.querySelector('.add-contact-btn');
     
     addContactBtn.addEventListener('click', function() {
-        // Создаем VCF файл (vCard)
-        const contactData = [
-            'BEGIN:VCARD',
-            'VERSION:3.0',
-            'FN:Кондратьев Никита',
-            'TEL;TYPE=CELL:+79992059681',
-            'EMAIL:nik070297@mail.ru',
-            'URL:https://niardm.github.io/design_kondrateva/',
-            'X-SOCIALPROFILE;TYPE=telegram:@design_kondrateva',
-            'X-SOCIALPROFILE;TYPE=whatsapp:+79992059681',
-            'X-SOCIALPROFILE;TYPE=vk:https://m.vk.com/design_kondrateva?from=groups',
-            'PHOTO;VALUE=URL;TYPE=PNG:contact_photo.png',
-            'NOTE:Дизайн Кондратьева',
-            'END:VCARD'
-        ].join('\n');
-        
-        // Создаем blob и ссылку для скачивания
-        const blob = new Blob([contactData], { type: 'text/vcard' });
+        // Формируем содержимое vCard с правильными переносами строк и указанием кодировки UTF-8
+        const vCardContent = `BEGIN:VCARD
+VERSION:3.0
+FN;CHARSET=UTF-8:Кондратьев Никита
+N;CHARSET=UTF-8:Кондратьев;Никита;;;
+TEL;TYPE=CELL:+79992059681
+EMAIL:nik070297@mail.ru
+URL:https://niardm.github.io/design_kondrateva/
+X-SOCIALPROFILE;TYPE=telegram:@design_kondrateva
+X-SOCIALPROFILE;TYPE=whatsapp:+79992059681
+X-SOCIALPROFILE;TYPE=vk:https://m.vk.com/design_kondrateva?from=groups
+PHOTO;VALUE=URL;TYPE=PNG:contact_photo.png
+NOTE;CHARSET=UTF-8:Дизайн Кондратьева
+END:VCARD`;
+
+        // Создаем Blob с явным указанием кодировки UTF-8
+        const blob = new Blob([vCardContent], {
+            type: 'text/vcard;charset=utf-8', // Явно указываем UTF-8
+            endings: 'native'
+        });
+
+        // Создаем ссылку для скачивания
         const url = URL.createObjectURL(blob);
-        
-        // Создаем временную ссылку
         const link = document.createElement('a');
         link.href = url;
-        link.download = 'Кондратьев_Никита.vcf';
+        link.download = 'contact.vcf';
         link.style.display = 'none';
         
-        // Добавляем на страницу и эмулируем клик
         document.body.appendChild(link);
         link.click();
         
-        // Убираем ссылку после скачивания
+        // Очистка
         setTimeout(() => {
             document.body.removeChild(link);
             URL.revokeObjectURL(url);
         }, 100);
-        
-        // Меняем текст кнопки на короткое время
+
+        // Визуальный фидбек
         const originalText = addContactBtn.innerHTML;
-        addContactBtn.innerHTML = '<i class="fas fa-check"></i> Сохранение контакта...';
-        
+        addContactBtn.innerHTML = '<i class="fas fa-check"></i> Контакт сохранён!';
         setTimeout(() => {
             addContactBtn.innerHTML = originalText;
         }, 2000);
