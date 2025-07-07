c2df8888e8237644be4e2b5d1918509d6372c01f
NiarDm <158210686+NiarDm@users.noreply.github.com>
2025-07-07 21:01:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const addContactBtn = document.querySelector('.add-contact-btn');
-    
+
     addContactBtn.addEventListener('click', function() {
-        // Формируем содержимое vCard
+        // Содержимое vCard обновлено для лучшей совместимости
         const vCardContent = `BEGIN:VCARD
-VERSION:2.1
-CHARSET=UTF-8
+VERSION:3.0
 FN;CHARSET=UTF-8:Кондратьев Никита
 N;CHARSET=UTF-8:Кондратьев;Никита;;;
 TEL;TYPE=CELL:+79992059681
-EMAIL:nik070297@mail.ru
+EMAIL;TYPE=INTERNET:nik070297@mail.ru
 URL:https://niardm.github.io/design_kondrateva/
-X-SOCIALPROFILE;TYPE=telegram:https://t.me/design_kondrateva
-X-SOCIALPROFILE;TYPE=whatsapp:https://wa.me/79992059681
-NOTE;CHARSET=UTF-8:Дизайн Кондратьева
+NOTE;CHARSET=UTF-8:Telegram: https://t.me/design_kondrateva\\nWhatsApp: https://wa.me/79992059681\\nДизайн Кондратьева
 END:VCARD`;
 
-        // Создаем Blob с явным указанием кодировки UTF-8
         const blob = new Blob([vCardContent], {
-            type: 'text/vcard;charset=utf-8', 
-            endings: 'native'
+            type: 'text/vcard;charset=utf-8'
         });
 
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
-        link.download = 'contact.vcf';
+        link.download = 'Kondratiev_Nikita.vcf';
         link.style.display = 'none';
-        
+
         document.body.appendChild(link);
         link.click();
-        
+
         setTimeout(() => {
             document.body.removeChild(link);
             URL.revokeObjectURL(url);
